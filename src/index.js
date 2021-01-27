2e01e868ec05cfdb4e2e346de71ea259b3d0145c
Valentina-Olekhnovich <volekh@gmail.com>
2021-01-27 18:48:15 +0100

    Update import and add Provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer)
+
+ReactDOM.render(
+    //Wrap main component inside Provider
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+) 
+ 10  src/reducers
