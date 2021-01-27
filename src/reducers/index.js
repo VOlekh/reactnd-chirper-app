99eef18c4de495df53ef50ad0aa135da2c80f70b
Valentina-Olekhnovich <volekh@gmail.com>
2021-01-27 18:47:09 +0100

    Use ES6 to combine all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,16 @@ import users from './users'
 import tweets from './tweets'
 
 //combine all of these reducers into one main, root reducer, the createStore function only accepts a single reducer
-export default combineReducers ({
-    authedUser: authedUser,
-    tweets: tweets,
-    users: users
-})
+// export default combineReducers ({
+//     authedUser: authedUser,
+//     tweets: tweets,
+//     users: users
+// })
+
+// Or using ES6's property shorthand, it can just be:
+
+export default combineReducers({
+  authedUser,
+  tweets,
+  users
+});
