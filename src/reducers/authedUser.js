7f9b853e260d56d2352b9dff4f4fb77dbf0387c0
Valentina-Olekhnovich <volekh@gmail.com>
2021-01-27 18:59:22 +0100

    Fix mistype

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,9 +1,9 @@
-import {SET_AUTHED_USERS} from "../actions/authedUser";
+import {SET_AUTHED_USER} from "../actions/authedUser.js";
 //And, the authedUser slice of the state in the store has been initialized to null.
 // state will initially be null, first time the function invoked  with the state of undefined
 export default function authedUser(state = null, action) {
   switch (action.type) {
-    case SET_AUTHED_USERS:
+    case SET_AUTHED_USER:
       return   action.id 
     default:
       return state;
